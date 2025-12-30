5f4a4a61cdf5f0e1ac163ac8fc7da55e725e5eb0
Felipe Bianchi Stefens <lipebianchistefens@gmail.com>
2025-12-29 22:29:38 -0300

    Adding .env fil and configuring it on the server in java

--- backend/scarlet_shade/src/main/java/com/server/scarlet_shade/ScarletShadeApplication.java
+++ backend/scarlet_shade/src/main/java/com/server/scarlet_shade/ScarletShadeApplication.java
@@ -1,13 +1,18 @@
 package com.server.scarlet_shade;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import com.server.scarlet_shade.configuration.DotEnvInitializer;
 
 @SpringBootApplication
 public class ScarletShadeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScarletShadeApplication.class, args);
-	}
 
+		new SpringApplicationBuilder(ScarletShadeApplication.class)
+			.initializers(new DotEnvInitializer())
+			.run(args);
+	}
+	
 }
---
+++ backend/scarlet_shade/src/main/java/com/server/scarlet_shade/configuration/DotEnvInitializer.java
@@ -0,0 +1,26 @@
+package com.server.scarlet_shade.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class DotEnvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>{
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+
+        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+        Dotenv dotenv = Dotenv.load();
+
+        Map<String, Object> dotenvProperties = new HashMap<>();
+        dotenv.entries().forEach(entry -> dotenvProperties.put(entry.getKey(), entry.getValue()));
+
+        environment.getPropertySources().addFirst(new MapPropertySource("dotenvProperties", dotenvProperties));
+    }
+}
