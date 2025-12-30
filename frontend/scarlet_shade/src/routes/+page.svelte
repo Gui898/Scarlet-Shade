0029c398239cf2f13cf7121de3b7a596fcc71301
Gui898 <gui.miojo.lasanha@gmail.com>
2025-12-30 14:00:29 -0300

    fixing the main screen

--- frontend/scarlet_shade/src/routes/+page.svelte
+++ frontend/scarlet_shade/src/routes/+page.svelte
@@ -1,20 +1,17 @@
 <script>
-    
-    import "$style/commum.css";
     import "$style/styles.css";
 
     import initialSamurai from "$assets/images/initialSamurai.png";
     import samurai1 from "$assets/images/samurai1.png";
     import samurai3 from "$assets/images/samurai3.png";
     import fog from "$assets/images/fog.png";
-
 </script>
 
-<div class="title">
-    <h1>Scarlet Shade</h1>
-</div>
-
 <div class="left_side">
+    
+    <div class="title">
+        <h1>Scarlet <br />Shade</h1>
+    </div>
 
     <div class="options">
         <button class="main_buttons">Login</button>
@@ -23,11 +20,11 @@
 </div>
 
 <div class="right_side">
-    <img class="samurai__Image img1" src="{initialSamurai}" alt="">
-    <img class="samurai__Image img2" src="{samurai1}" alt="">
-    <img class="samurai__Image img3" src="{samurai3}" alt="">
+    <img class="samurai__Image img1" src={initialSamurai} alt="" />
+    <img class="samurai__Image img2" src={samurai1} alt="" />
+    <img class="samurai__Image img3" src={samurai3} alt="" />
 </div>
 
 {#each Array(5) as fogImage, i}
-    <img src="{fog}" alt="" class="fog" style="--i:{i+1}">
+    <img src={fog} alt="" class="fog" style="--i:{i + 1}" />
 {/each}
