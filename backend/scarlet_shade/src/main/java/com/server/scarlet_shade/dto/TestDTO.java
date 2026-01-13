84631b1901af482c8e86ef21df8134850eb6df9c
Gui898 <gui.miojo.lasanha@gmail.com>
2026-01-12 22:01:00 -0300

    Fixing the button, I hate css

--- backend/scarlet_shade/src/main/java/com/server/scarlet_shade/dto/TestDTO.java
+++
@@ -1,5 +0,0 @@
-package com.server.scarlet_shade.dto;
-
-public class TestDTO {
-    
-}
--- frontend/scarlet_shade/src/routes/+page.svelte
+++ frontend/scarlet_shade/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-
     import "$style/pages/homeStyle.css";
     import "$style/components/buttonStyle.css";
     import "$style/components/fogStyle.css";
@@ -16,6 +15,17 @@
 
     let screen = "options";
 
+    function fadeScale(node, { delay = 0, duration = 400, start = 0.6 }) {
+        return {
+            delay,
+            duration,
+            css: (t) => `
+            opacity: ${t};
+            transform: scale(${start + (1 - start) * t});
+        `,
+        };
+    }
+
     function goLogin() {
         screen = "login";
     }
@@ -30,65 +40,85 @@
 </script>
 
 <div class="left_side">
-
     {#if screen !== "options"}
-        <button 
-            onclick={back}
-            class="back_arrow">
-            ←
-        </button>
+        <button onclick={back} class="back_arrow"> ← </button>
     {/if}
-    
+
     <div class="title">
-        <h1>Scarlet <br/>Shade</h1>
+        <h1>Scarlet <br />Shade</h1>
     </div>
 
     <div class="content_area">
-
         <div class="options buttons">
-            <button 
-                class="button" 
-                class:button_offscreen={screen !== 'options'}
-                onclick={goLogin}>
-                    
-                <img src={arrowButton} alt="" class="arrow">
+            <button
+                class="button"
+                class:button_offscreen={screen !== "options"}
+                onclick={goLogin}
+            >
+                <img src={arrowButton} alt="" class="arrow" />
                 Login
             </button>
 
-            <button 
+            <button
                 class="button"
-                class:button_offscreen={screen !== 'options'}
-                onclick={goRegister}>
-
-                <img src={arrowButton} alt="" class="arrow">
+                class:button_offscreen={screen !== "options"}
+                onclick={goRegister}
+            >
+                <img src={arrowButton} alt="" class="arrow" />
                 Register
             </button>
         </div>
-        
-        {#if screen === "login"}
 
-            <form method="POST" action="?/login" class="options login">
-                
-                <input name="username" type="text" placeholder="Username" bind:value={username}>
-                <input name="password" type="password" placeholder="Password" bind:value={password}>
-                
+        {#if screen === "login"}
+            <form
+                transition:fadeScale={{ duration: 400, delay: 600 }}
+                method="POST"
+                action="?/login"
+                class="options login"
+            >
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    bind:value={username}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    bind:value={password}
+                />
+
                 <button type="submit" class="button">
-                    <img src={arrowButton} alt="" class="arrow">
+                    <img src={arrowButton} alt="" class="arrow" />
                     Login
                 </button>
             </form>
         {/if}
 
         {#if screen === "register"}
-            
             <form method="POST" action="?/register" class="options register">
-                
-                <input name="username" type="text" placeholder="Username" bind:value={username}>
-                <input name="email" type="email" placeholder="Email" bind:value={email}>
-                <input name="password" type="password" placeholder="Password" bind:value={password}>
-                
+                <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    bind:value={username}
+                />
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                    bind:value={email}
+                />
+                <input
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    bind:value={password}
+                />
+
                 <button type="submit" class="button">
-                    <img src={arrowButton} alt="" class="arrow">
+                    <img src={arrowButton} alt="" class="arrow" />
                     Register
                 </button>
             </form>
@@ -97,14 +127,24 @@
 </div>
 
 <div class="right_side">
-
     <!-- Using all images. If use more, create a For loop in Svelte -->
-    <img class="samurai_image samurai_image_1" src={samuraiImage1} alt="samurai"/>
-    <img class="samurai_image samurai_image_2" src={samuraiImage2} alt="samurai"/>
-    <img class="samurai_image samurai_image_3" src={samuraiImage3} alt="samurai"/>
+    <img
+        class="samurai_image samurai_image_1"
+        src={samuraiImage1}
+        alt="samurai"
+    />
+    <img
+        class="samurai_image samurai_image_2"
+        src={samuraiImage2}
+        alt="samurai"
+    />
+    <img
+        class="samurai_image samurai_image_3"
+        src={samuraiImage3}
+        alt="samurai"
+    />
 </div>
 
 {#each Array(5) as fog, i}
-
     <img src={fogImage} alt="" class="fog" style="--i:{i + 1}" />
 {/each}
