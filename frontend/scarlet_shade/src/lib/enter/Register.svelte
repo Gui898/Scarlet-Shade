46b3a20f13e647566c42ff888a122b89ef136208
Gui898 <gui.miojo.lasanha@gmail.com>
2026-01-29 19:08:58 -0300

    fixing the overpostion on regex messages

--- frontend/scarlet_shade/src/lib/enter/Register.svelte
+++ frontend/scarlet_shade/src/lib/enter/Register.svelte
@@ -1,5 +1,4 @@
 <script>
-
     import "$style/pages/homeStyle.css";
     import "$style/components/buttonStyle.css";
 
@@ -15,36 +14,38 @@
 
     const usernameRegex = /^[a-zA-Z0-9._-]{8,}$/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/; 
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$/;
 
     $: wrongUsername = username.length > 0 && !usernameRegex.test(username);
     $: wrongEmail = email.length > 0 && !emailRegex.test(email);
     $: wrongPassword = password.length > 0 && !passwordRegex.test(password);
 
-    $: isFormValid = wrongPassword ||
-                     wrongEmail ||
-                     wrongUsername ||
-                     username.length == 0 ||
-                     email.length == 0 ||
-                     password.length == 0;
+    $: warningMessage = wrongUsername
+        ? "The username must have at least 8 characters"
+        : wrongEmail
+        ? "The email must be in the correct format"
+        : wrongPassword
+        ? "The password must be at least 8 characters, with uppercase and lowercase letters"
+        : null;
 
+    $: isFormValid =
+        wrongPassword ||
+        wrongEmail ||
+        wrongUsername ||
+        username.length == 0 ||
+        email.length == 0 ||
+        password.length == 0;
 </script>
 
 <Warning></Warning>
 <form method="POST" action="?/register" class="options register">
-    
     <input
         name="username"
         type="text"
         placeholder="Username"
         bind:value={username}
     />
-    <input
-        name="email"
-        type="email"
-        placeholder="Email"
-        bind:value={email}
-    />
+    <input name="email" type="email" placeholder="Email" bind:value={email} />
     <input
         name="password"
         type="password"
@@ -52,22 +53,20 @@
         bind:value={password}
     />
 
-    <button type="submit" class="button" onclick={() => playSound(gongSound)} disabled={isFormValid}>
-        <img src={arrowButton} alt="" class="arrow"/>
+    <button
+        type="submit"
+        class="button"
+        onclick={() => playSound(gongSound)}
+        disabled={isFormValid}
+    >
+        <img src={arrowButton} alt="" class="arrow" />
         Register
     </button>
 </form>
 
-{#if wrongUsername}
+{#if warningMessage}
     <Warning>
-        <h6>The name should be between 10 and 20 characters long</h6>
+        <h6>{warningMessage}</h6>
     </Warning>
 {/if}
 
-{#if wrongEmail}
-    <Warning><h6>The email must be in the correct format</h6></Warning>
-{/if}
-
-{#if wrongPassword}
-    <Warning><h6>The password should be 8 characters long, at least 1 capital letter and 1 lowercase letter</h6></Warning>
-{/if}
