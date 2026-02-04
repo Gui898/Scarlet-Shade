832567508542a6b992120171e32a25e9e08d916b
Gui898 <gui.miojo.lasanha@gmail.com>
2026-02-04 01:45:11 -0300

    implementing lerp

--- frontend/scarlet_shade/src/script/models/player/Camera.js
+++ frontend/scarlet_shade/src/script/models/player/Camera.js
@@ -3,15 +3,27 @@ import { GameObject } from "../GameObject";
 export class Camera extends GameObject{
 
     canvas;
+    smoothness = 0.1;
 
     constructor(canvas, posX, posY){
         super(canvas.width, canvas.height, posX, posY);
         this.canvas = canvas;
     }
 
-    updateCamera(player){
-        this.position.x = player.position.x + player.width/2 - this.width/2; 
-        this.position.y = player.position.y + player.height/2 - this.height/2; 
+    // a = currentPosition, b = targetPosition, t = smoothness
+    lerp(a, b, t){
+        return a + (b-a) * t
+    } 
+
+    updateCamera(player) {
+        const targetX =
+            player.position.x + player.width / 2 - this.width / 2;
+
+        const targetY =
+            player.position.y + player.height / 2 - this.height / 2;
+
+        this.position.x = this.lerp(this.position.x, targetX, this.smoothness);
+        this.position.y = this.lerp(this.position.y, targetY, this.smoothness);
     }
 
     draw(ctx){
